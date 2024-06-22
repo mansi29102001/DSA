274a906b0856a7495c80ee81a52187e5cc40e95a
Mansi <mansibhondave@gmail.com>
2024-06-22 15:41:16 +0530

    Largest and second largest element in array ques

--- src/array/LargestElement.java
+++ src/array/LargestElement.java
@@ -2,7 +2,7 @@ package array;
 
 public class LargestElement {
     public static void main(String[] args) {
-        int arr[] = {8,10,5,7,9};
+        int[] arr = {8,10,5,7,9};
         int max=arr[0];
         for(int i=1;i<arr.length;i++){
             if(max<arr[i]){
