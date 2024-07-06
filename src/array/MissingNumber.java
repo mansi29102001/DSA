c3e29f20e5bbda2e88b4815ef9c6b1918dba8930
Mansi <mansibhondave@gmail.com>
2024-07-06 21:28:13 +0530

    optimised Missing number

--- src/array/MissingNumber.java
+++ src/array/MissingNumber.java
@@ -4,22 +4,18 @@ import java.util.Arrays;
 
 public class MissingNumber {
     public static void main(String[] args) {
-        int[] nums = {8,6,4,2,3,5,7,0,1};
+        int[] nums = {3,0,1};
         System.out.println(missingNumber(nums));
     }
 
     public static int missingNumber(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        int i;
-        for(i=0;i<nums.length;i++){
-            if(i!=nums[i]){
-                return i;
-            }
+        int sumNums=0;
+        int sumArray=0;
+        sumNums = (n*(n+1))/2;
+        for(int i=0;i<n;i++){
+            sumArray += nums[i];
         }
-        if(i==n){
-            return i;
-        }
-        return -1;
+        return sumNums-sumArray;
     }
 }
